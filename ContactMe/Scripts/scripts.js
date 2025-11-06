5f95d54a5915457f2348e1a2b9289bdc72012d96
JH2318 <227825031+JH2318@users.noreply.github.com>
2025-11-06 09:17:29 -0600

    Add files via upload

--- ContactMe/Scripts/scripts.js
+++ ContactMe/Scripts/scripts.js
@@ -23,11 +23,45 @@
                             .map(cb => cb.value)
                             .join(", ") || "None";
 
-      // Display everything in an alert
-      alert(
-        `--- CONTACT FORM DATA ---\n\n` +
-        `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
+    // Create an array of key value pairs
+    const formData = [
+      ["Name", name],
+      ["Email, email"],
+      ["Password", password],
+      ["Phone", phone],
+      ["Website", website],
+      ["Age", age],
+      ["Date of Birth", dob]
+      ["Preferred Time", time],
+      ["Favorite Color", color],
+      ["Interest Level", range],
+      ["Contact Method", contactMethod],
+      ["Services Interested In", services],
+      ["Topic", topic],
+      ["Message", message]
+    ];
+
+      //show the results section
+      const resultContainer = 
+      document.getElementById("#resultContainer");
+      const resultBody = document.querySelector("#resultTable tbody");
+      resultBody.innerHTML ="";
+      resultContainer.style.display = "flex";
+
+            //Populate table
+      formData.forEach (([key, value]) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>${key}</td><td>${value}</td>`;
+      resultBody.appendChild(row);
+}
       );
+
+     //Old assignment alert
+     // Display everything in an alert
+      // alert(
+      //   `--- CONTACT FORM DATA ---\n\n` +
+      //   `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
+      // );
 
       // Reset the form
       this.reset();
